0bdcadfa5a91246fc5ad48bbd9afcdf4066b099b
mohammadreza <mamadm118@gmail.com>
2026-07-29 13:54:56 +0330

    use context edded

--- apps/frontend/Contexts/CartContext.jsx
+++ apps/frontend/Contexts/CartContext.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
